689929c3e998012689771d4e5f3fd524bb197ce8
Michał Kalbarczyk <fazibear@gmail.com>
2017-01-21 18:49:05 +0100

    Update deps

--- mix.exs
+++ mix.exs
@@ -13,28 +13,18 @@ defmodule Exrbbench.Mixfile do
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
-  def application do
+  def application() do
     [
       applications: [:logger],
       mod: {Exrbbench, []}
     ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:benchee, "~> 0.6"},
       {:benchee_html, "~> 0.1"},
-      {:export, "~> 0.0.7"},
-      {:erlport, github: "hdima/erlport", manager: :make}
+      {:export, "~> 0.1"},
     ]
   end
 end
